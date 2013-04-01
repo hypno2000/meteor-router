4c2a3c590839e33fa4a9f3e651b75d32550097d3
Reio Piller <reio@heathmont.net>
2013-04-01 22:45:41 +0300

    dev

--- router_server.js
+++ router_server.js
@@ -160,14 +160,14 @@
   Meteor.Router = new Router();
   
   // hook up the serving
-  var connect = __meteor_bootstrap__.require("connect");
+  var connect = Npm.require("connect");
   __meteor_bootstrap__.app
     .use(connect.query()) // <- XXX: we can probably assume accounts did this
     .use(connect.bodyParser())
     .use(function(req, res, next) {
       // need to wrap in a fiber in case they do something async 
       // (e.g. in the database)
-      Fiber(function() {
+		  Npm.require("fibers").Fiber(function() {
         var output = Meteor.Router.match(req, res);
         
         if (output === false) {
@@ -205,5 +205,5 @@
       }).run();
     });
 
-}())
+}());
 
